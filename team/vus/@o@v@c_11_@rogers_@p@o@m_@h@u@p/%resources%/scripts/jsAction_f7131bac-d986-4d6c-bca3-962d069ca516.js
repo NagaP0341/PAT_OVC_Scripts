e570b1b6c2ba0b9c833dca45829f2bedaa4c28ed
N.palem <N.Palem@rci.rogers.com>
2025-03-17 16:18:47 -0400

    OVC_11_Rogers_POM_HUP enhancement completed. Assertions yet to add

---
+++ team/vus/@o@v@c_11_@rogers_@p@o@m_@h@u@p/%resources%/scripts/jsAction_f7131bac-d986-4d6c-bca3-962d069ca516.js
@@ -0,0 +1,22 @@
+﻿// Get variable value from VariableManager
+var GetPromoDetailssResp = context.variableManager.getValue("C_GetPromoDetails");
+var GetPromoDetails_JSON = JSON.parse(GetPromoDetailssResp);
+
+var promoDetails = GetPromoDetails_JSON['promoDetails'];
+var externalId = promoDetails['externalId'];
+var promoId = promoDetails['promoId'];
+var promoName = promoDetails['promoName'];
+ 
+// Inject the computed value in a runtime variable
+   context.variableManager.setValue("C_PromodetailsJS_ExternalID3",externalId);//RP002PA2R
+   context.variableManager.setValue("C_PromodetailsJS_PromoID",promoId);  //51450367-90dd-4a34-809d-d8adadf6a082
+   context.variableManager.setValue("C_PromodetailsJS_PromoName",promoName);  //(PATTESTB) PAT_23PI2_SetB_05
+   
+// Do some computation using the methods
+ 
+// outpuvalue
+logger.debug("C_PromodetailsJS_ExternalID3="+externalId);
+logger.debug("C_PromodetailsJS_PromoID="+promoId);
+logger.debug("C_PromodetailsJS_PromoName="+promoName);
+ 
+//context.variableManager.setValue("computedVar",computedValue);
