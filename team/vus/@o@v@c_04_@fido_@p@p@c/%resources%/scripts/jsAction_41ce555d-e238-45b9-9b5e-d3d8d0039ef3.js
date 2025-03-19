7adb3e5f4385d0573c22de927d7b454e9998a87a
Priyanka.Pawar2 <priyanka.pawar2@rci.rogers.ca>
2025-03-19 10:13:31 -0400

    Fixed OVC Fido PPC scirpt

---
+++ team/vus/@o@v@c_04_@fido_@p@p@c/%resources%/scripts/jsAction_41ce555d-e238-45b9-9b5e-d3d8d0039ef3.js
@@ -0,0 +1,22 @@
+﻿//Below line is the mandatory file for write file to work in Neoload
+var lock= new java.util.concurrent.locks.ReentrantLock();
+//writes users company, userid and script they're executing.
+function writeFile(){
+var GesID = context.variableManager.getValue("P_OVC_04_Fido_PPC.P_BAN");
+//var IMEI = context.variableManager.getValue("C_CTN1");
+ 
+lock.lock();
+//Open the file for writing
+//var writer= new java.io.FileWriter("D:\\Bell Canada Current\\NM1 R46\\FINAL\\TestData\\UNLOCK.GESID_13Jan.txt",true);
+var writer= new java.io.FileWriter("D:\\FileWrite\\OVC_04_Fido_PPC_BAn.txt",true);
+writer.write("" + GesID +"\r\n");
+//writer.write("" + GesID +","+ IMEI + "\r\n");
+//writer.write("" + GesID +"\r\n");
+//Close the file
+//writer.close();
+//Close the file
+writer.close();
+//Unlock the lock so that other users can write on it
+lock.unlock();
+}
+writeFile();
--- team/vus/@o@v@c_04_@fido_@p@p@c/%resources%/scripts/jsAction_5c5ef8ce-4521-468c-9a41-1c8a7a51eb5b.js
+++ team/vus/@o@v@c_04_@fido_@p@p@c/%resources%/scripts/jsAction_5c5ef8ce-4521-468c-9a41-1c8a7a51eb5b.js
@@ -2,15 +2,20 @@
 var AddonsResp = context.variableManager.getValue("C_HupAddons_Details");
 var Addons_JSON = JSON.parse(AddonsResp);
 
-// var longDistance = Addons_JSON['longDistance'];
-// var addons = longDistance['addons'];
-// var addons_externalId = addons[0]['externalId']; 
-// var addons_skuId = addons[0]['skuId'];
+var offers = Addons_JSON['longDistance'];
+var addons = offers['addons'];
+var addons_externalId = addons[0]['externalId']; 
+ var addons_skuId = addons[0]['skuId'];
+ 
+ var voiceMail = Addons_JSON['longDistance'];
+var voiceMail_addons = voiceMail['addons'];
+var voiceMail_addons_externalId = voiceMail_addons[0]['externalId']; 
+ var voiceMail_addons_skuId = voiceMail_addons[0]['skuId'];
 
 
-var existing = Addons_JSON['existing'];
-var existing_addons = existing['addons'];
-var existing_externalId = existing_addons[0]['externalId'];
+//var existing = Addons_JSON['existing'];
+//var existing_addons = existing['addons'];
+//var existing_externalId = existing_addons[0]['externalId'];
 // var existing_externalId1 = existing_addons[1]['externalId'];
  
  
@@ -18,8 +23,12 @@ var existing_externalId = existing_addons[0]['externalId'];
 //    context.variableManager.setValue("C_addons_externalId",addons_externalId);//NOPROTECTION
 //    context.variableManager.setValue("C_addons_skuId",addons_skuId);  //NOPROTECTION
 
-    context.variableManager.setValue("C_existing_externalId",existing_externalId);  //FULLDETF
-//     context.variableManager.setValue("C_existing_externalId1",existing_externalId1);  //OPTSRA1AK
+    context.variableManager.setValue("C_addons_externalId",voiceMail_addons_externalId);  //FULLDETF
+context.variableManager.setValue("C_addons_skuId",addons_skuId);  //OPTSRA1AK
+
+    context.variableManager.setValue("C_voiceMail_addons_externalId",voiceMail_addons);  //FULLDETF
+context.variableManager.setValue("C_voicemail_addons_skuId",voiceMail_addons_skuId);  //OPTSRA1AK
+ 
  
         
 // Do some computation using the methods
@@ -28,5 +37,5 @@ var existing_externalId = existing_addons[0]['externalId'];
 
 // logger.debug("addons_externalId="+addons_externalId);
 // logger.debug("addons_skuId="+addons_skuId);
-logger.debug("existing_externalId="+existing_externalId);
+//logger.debug("existing_externalId="+existing_externalId);
 // logger.debug("existing_externalId1="+existing_externalId1);
